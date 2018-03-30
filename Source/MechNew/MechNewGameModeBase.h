b1afe3c2a3aa9891ceafbda629c09573a557232c
git@github.com <t.chigau@gmail.com>
2018-03-30 22:42:22 +0200

    test

---
+++ Source/MechNew/MechNewGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MechNewGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MECHNEW_API AMechNewGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
